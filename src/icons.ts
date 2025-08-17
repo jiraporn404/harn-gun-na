e09f66ce7dde79764690ee722f1e0b96701cc1e5
jiraporn404 <chm.jiraporn@gmail.com>
2025-08-17 19:44:21 +0700

    Add icons in new file

---
+++ src/icons.ts
@@ -0,0 +1,6 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import HomeIcon from "@mui/icons-material/Home";
+import SaveAltIcon from "@mui/icons-material/SaveAlt";
+
+export { DeleteIcon, EditIcon, HomeIcon, SaveAltIcon };
